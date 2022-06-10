663204db4f33dbfa5e362f669c72bd8ccd2418c7
Vincent Boutour <bob@vibioh.fr>
2022-06-10 23:58:03 +0200

    refactor: Using generics to reduce lines count
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/advanced/advanced.go
+++ cmd/advanced/advanced.go
@@ -5,21 +5,24 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/ViBiOh/flags"
 )
 
 type databaseConfig struct {
-	url  *string
-	port *uint
-	name *string
+	url     *string
+	port    *uint
+	name    *string
+	timeout *time.Duration
 }
 
 func databaseFlags(fs *flag.FlagSet, prefix string, overrides ...flags.Override) databaseConfig {
 	return databaseConfig{
-		url:  flags.String(fs, prefix, "db", "Url", "Database url", "", overrides),
-		port: flags.Uint(fs, prefix, "db", "Port", "Database port", 5432, overrides),
-		name: flags.String(fs, prefix, "db", "Name", "Database name", "user", overrides),
+		url:     flags.String(fs, prefix, "db", "Url", "Database url", "", overrides),
+		port:    flags.Uint(fs, prefix, "db", "Port", "Database port", 5432, overrides),
+		name:    flags.String(fs, prefix, "db", "Name", "Database name", "user", overrides),
+		timeout: flags.Duration(fs, prefix, "db", "Timeout", "Request timeout", time.Second, overrides),
 	}
 }
 
@@ -28,12 +31,16 @@ func databaseFlags(fs *flag.FlagSet, prefix string, overrides ...flags.Override)
 //         [main] Database name {MY_CLI_MAIN_NAME} (default "user")
 //   -mainPort uint
 //         [main] Database port {MY_CLI_MAIN_PORT} (default 5432)
+//   -mainTimeout duration
+//         [main] Request timeout {MY_CLI_MAIN_TIMEOUT} duration (default 1s)
 //   -mainUrl string
 //         [main] Database url {MY_CLI_MAIN_URL}
 //   -replicaName string
 //         [replica] Database name {MY_CLI_REPLICA_NAME} (default "user-replica")
 //   -replicaPort uint
 //         [replica] Database port {MY_CLI_REPLICA_PORT} (default 5432)
+//   -replicaTimeout duration
+//         [replica] Request timeout {MY_CLI_REPLICA_TIMEOUT} duration (default 1s)
 //   -replicaUrl string
 //         [replica] Database url {MY_CLI_REPLICA_URL}
 
@@ -49,4 +56,5 @@ func main() {
 
 	fmt.Printf("Main name=`%s`\n", *mainConfig.name)
 	fmt.Printf("Replica name=`%s`\n", *replicaConfig.name)
+	fmt.Printf("Timeout name=`%s`\n", *replicaConfig.timeout)
 }
--- flag.go
+++ flag.go
@@ -26,49 +26,63 @@ func NewOverride(name string, value any) Override {
 // String creates a string flag
 func String(fs *flag.FlagSet, prefix, docPrefix, name, label string, value string, overrides []Override) *string {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.String(FirstLowerCase(flagName), LookupEnvString(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.String(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (string, error) {
+		return input, nil
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Int creates a int flag
 func Int(fs *flag.FlagSet, prefix, docPrefix, name, label string, value int, overrides []Override) *int {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Int(FirstLowerCase(flagName), LookupEnvInt(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Int(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (int, error) {
+		intVal, err := strconv.ParseInt(input, 10, 32)
+		return int(intVal), err
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Int64 creates a int64 flag
 func Int64(fs *flag.FlagSet, prefix, docPrefix, name, label string, value int64, overrides []Override) *int64 {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Int64(FirstLowerCase(flagName), LookupEnvInt64(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Int64(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (int64, error) {
+		return strconv.ParseInt(input, 10, 64)
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Uint creates a uint flag
 func Uint(fs *flag.FlagSet, prefix, docPrefix, name, label string, value uint, overrides []Override) *uint {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Uint(FirstLowerCase(flagName), LookupEnvUint(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Uint(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (uint, error) {
+		intVal, err := strconv.ParseUint(input, 10, 32)
+		return uint(intVal), err
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Uint64 creates a uint64 flag
 func Uint64(fs *flag.FlagSet, prefix, docPrefix, name, label string, value uint64, overrides []Override) *uint64 {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Uint64(FirstLowerCase(flagName), LookupEnvUint64(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Uint64(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (uint64, error) {
+		return strconv.ParseUint(input, 10, 64)
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Float64 creates a float64 flag
 func Float64(fs *flag.FlagSet, prefix, docPrefix, name, label string, value float64, overrides []Override) *float64 {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Float64(FirstLowerCase(flagName), LookupEnvFloat64(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Float64(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, func(input string) (float64, error) {
+		return strconv.ParseFloat(input, 64)
+	}), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Bool creates a bool flag
 func Bool(fs *flag.FlagSet, prefix, docPrefix, name, label string, value bool, overrides []Override) *bool {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Bool(FirstLowerCase(flagName), LookupEnvBool(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Bool(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, strconv.ParseBool), formatLabel(prefix, docPrefix, label, envName))
 }
 
 // Duration creates a duration flag
 func Duration(fs *flag.FlagSet, prefix, docPrefix, name, label string, value time.Duration, overrides []Override) *time.Duration {
 	flagName, envName := getNameAndEnv(fs, FirstUpperCase(prefix), name)
-	return fs.Duration(FirstLowerCase(flagName), LookupEnvDuration(name, envName, value, overrides), formatLabel(prefix, docPrefix, label, envName))
+	return fs.Duration(FirstLowerCase(flagName), lookupDefaultValue(name, envName, value, overrides, time.ParseDuration), formatLabel(prefix, docPrefix, label, envName))
 }
 
 func getNameAndEnv(fs *flag.FlagSet, prefix, name string) (string, string) {
@@ -92,100 +106,14 @@ func formatLabel(prefix, docPrefix, label, envName string) string {
 	return builder.String()
 }
 
-// LookupEnvString search for given key in environment
-func LookupEnvString(name, envName string, value string, overrides []Override) string {
+func lookupDefaultValue[T any](name, envName string, value T, overrides []Override, parse func(string) (T, error)) T {
 	if val, ok := os.LookupEnv(envName); ok {
-		return val
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvInt search for given key in environment as int
-func LookupEnvInt(name, envName string, value int, overrides []Override) int {
-	if val, ok := os.LookupEnv(envName); ok {
-		intVal, err := strconv.ParseInt(val, 10, 32)
+		parsed, err := parse(val)
 		if err == nil {
-			return int(intVal)
+			return parsed
 		}
 	}
 
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvInt64 search for given key in environment as int64
-func LookupEnvInt64(name, envName string, value int64, overrides []Override) int64 {
-	if val, ok := os.LookupEnv(envName); ok {
-		intVal, err := strconv.ParseInt(val, 10, 64)
-		if err == nil {
-			return intVal
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvUint search for given key in environment as uint
-func LookupEnvUint(name, envName string, value uint, overrides []Override) uint {
-	if val, ok := os.LookupEnv(envName); ok {
-		intVal, err := strconv.ParseUint(val, 10, 32)
-		if err == nil {
-			return uint(intVal)
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvUint64 search for given key in environment as uint64
-func LookupEnvUint64(name, envName string, value uint64, overrides []Override) uint64 {
-	if val, ok := os.LookupEnv(envName); ok {
-		intVal, err := strconv.ParseUint(val, 10, 64)
-		if err == nil {
-			return intVal
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvFloat64 search for given key in environment as float64
-func LookupEnvFloat64(name, envName string, value float64, overrides []Override) float64 {
-	if val, ok := os.LookupEnv(envName); ok {
-		floatVal, err := strconv.ParseFloat(val, 64)
-		if err == nil {
-			return floatVal
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvBool search for given key in environment as bool
-func LookupEnvBool(name, envName string, value bool, overrides []Override) bool {
-	if val, ok := os.LookupEnv(envName); ok {
-		boolBal, err := strconv.ParseBool(val)
-		if err == nil {
-			return boolBal
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-// LookupEnvDuration search for given key in environment as time.Duration
-func LookupEnvDuration(name, envName string, value time.Duration, overrides []Override) time.Duration {
-	if val, ok := os.LookupEnv(envName); ok {
-		boolBal, err := time.ParseDuration(val)
-		if err == nil {
-			return boolBal
-		}
-	}
-
-	return getOverridenValue(overrides, name, value)
-}
-
-func getOverridenValue[T any](overrides []Override, name string, value T) T {
 	for _, override := range overrides {
 		if strings.EqualFold(name, override.name) {
 			return override.value.(T)
